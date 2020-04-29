d43e9b19f58f7df390af74afff1a1810a7eb6dac
ValeBini <valentinabini98@gmail.com>
2020-04-29 10:43:50 -0300

    cambio 0 y 1 por CONSOLE_INPUT y CONSOLE_OUTPUT en probando

--- code/userland/probando.c
+++ code/userland/probando.c
@@ -1,13 +1,14 @@
 #include "syscall.h"
 
 int main(){
-	Write("Ingrese un nombre de maximo 10 char: ", 37, 1);
+	Write("Ingrese un nombre de maximo 10 char: ", 37, CONSOLE_OUTPUT);
 	char name[10];
-	int n = Read(name, 10, 0);
+	int n = Read(name, 10, CONSOLE_INPUT);
 
 	// Create("test.txt");
 	// OpenFileId id = Open("test.txt");
-	Write(name, n, 1);
+	Write(name, n, CONSOLE_OUTPUT);
+	Write("\n", 1, CONSOLE_OUTPUT);
 	// Close(id);
 	Halt();
 }
